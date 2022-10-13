1b5189118ac7ab33d6801d6985c4ab905dfb1de3
giorgip62 <giorgiop62@gmail.com>
2022-10-13 15:41:35 +0200

    ciclo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var numb;
+var x;
+var y;
+var z;
+// stampo i numeri da 1-100
+for(let numb=1; numb<=100; numb++){
+  console.log(numb);
+  if((numb % 3 == 0)||(numb % 5 == 0){
+    numb = x;
+    console.log(x);
+  }
+
+}
+
