2a1bcc9c448f562f0fbd73fd40878b7cf455e0cc
giorgip62 <giorgiop62@gmail.com>
2022-10-13 17:31:05 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -6,22 +6,24 @@ for( let i=1;i<=100; i++){
 
   if(( i % 3 == 0) && (i % 5 == 0)){
     box.classList.add('red');
-    box.innerHTML = ('fix');
+    box.innerHTML = 'fixbuz';
     console.log('fixbuz');
 
   }else if(i % 3 == 0){
     box.classList.add('green');
+    box.innerHTML = 'fix';
     console.log('fix');
 
   }else if ( i % 5 == 0){
     box.classList.add('yellow');
+    box.innerHTML = 'buz';
     console.log('buz');
 
   }else {
+    box.innerHTML = i;
     console.log(i);
   }
 
-  box.innerHTML = i;
   container.append(box);
 
 
