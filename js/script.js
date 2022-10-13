0e6d6a492590497a2f95a3120864348cfb7c163a
giorgip62 <giorgiop62@gmail.com>
2022-10-13 15:53:45 +0200

    fix ma nel punto sbagliato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-var numb;
-var x;
-var y;
-var z;
+
 // stampo i numeri da 1-100
-for(let numb=1; numb<=100; numb++){
-  console.log(numb);
-  if((numb % 3 == 0)||(numb % 5 == 0){
-    numb = x;
-    console.log(x);
+for(let i=1; i<=100; i++){
+  console.log(i);
+  if(i % 3 == 0){
+    console.log('fix');
   }
 
 }
