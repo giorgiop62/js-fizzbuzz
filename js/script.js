fe819f7e16ea40b4da1cf8ba481c593e9fbd0eba
giorgip62 <giorgiop62@gmail.com>
2022-10-13 16:59:08 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const container = document.querySelector('.container');
 
 for( let i=1;i<=100; i++){
-  if(( i % 3 == 0) && (i % 5 == 0)){
-    box.classList.add('red');
-    console.log('fizbuz');
-  }else if(i % 3 == 0){
-    box.classList.add('green');
-    console.log('fix');
-  }else if ( i % 5 == 0){
-    box.classList.add('yellow');
-    console.log('buz');
-  }else {
-    console.log(i);
-  }
   const box = document.createElement('div');
   box.className = 'box';
 
+  if(( i % 3 == 0) && (i % 5 == 0)){
+    box.classList.add('red');
+    console.log('fizbuz');
+
+  }else if(i % 3 == 0){
+    box.classList.add('green');
+    console.log('fix');
+
+  }else if ( i % 5 == 0){
+    box.classList.add('yellow');
+    console.log('buz');
+
+  }else {
+    console.log(i);
+  }
 
   box.innerHTML = i;
   container.append(box);
